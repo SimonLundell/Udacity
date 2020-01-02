4eb9184c87cc055a32fb53151ad843bf3855a0db
SimonLundell <simonlundell@hotmail.com>
2020-01-02 16:01:50 +0100

    Lesson 6 first try

--- LiAlg.py
+++ LiAlg.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Vector(object):
     # Creates vector based on an input list of coordinates and also sets the dimensions of the space
     # that the vector lives in.
@@ -21,28 +23,35 @@ class Vector(object):
     def minus(self, v):
         new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
         return new_coordinates
-    
+
     def times_scalar(self, c):
         new_coordinates = [c*x for x in self.coordinates]
         return Vector(new_coordinates)
     
+    def magnitude(self, v):
+        calc_magnitude = [sqrt((x*x) + (y*y) + (z*z)) for x,y,z in zip(self.coordinates, v.coordinates)]
+        return calc_magnitude
+
     def __str__(self):
         return 'Vector: {}' .format(self.coordinates)
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-v = Vector([8.218, -9.341])
-w = Vector([-1.129, 2.111])
-print(v.plus(w))
+v = Vector([1, 1, 2])
+print(v.magnitude(v))
 
-v = Vector([7.119, 8.215])
-w = Vector([-8.223, 0.878])
-print(v.minus(w))
+#v = Vector([8.218, -9.341])
+#w = Vector([-1.129, 2.111])
+#print(v.plus(w))
 
-v = Vector([1.671, -1.012, -0.318])
-c = 7.41
-print(v.times_scalar(c))
+#v = Vector([7.119, 8.215])
+#w = Vector([-8.223, 0.878])
+#print(v.minus(w))
+
+#v = Vector([1.671, -1.012, -0.318])
+#c = 7.41
+#print(v.times_scalar(c))
 
 #my_vector = Vector([1,2,3])
 #my_vector2 = Vector([1,2,3])
@@ -52,5 +61,3 @@ print(v.times_scalar(c))
 #print(my_vector==my_vector2)
 #print(my_vector==my_vector3)
 
-#Testing some stuff
-#Testing some more stuff
