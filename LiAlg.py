7b469f4f7118b3e42674453f41a92d72b0fbc596
SimonLundell <simonlundell@hotmail.com>
2020-01-05 19:02:11 +0100

    Lesson_13 complete. Note that exception messages are standardized responses in python being compared

--- LiAlg.py
+++ LiAlg.py
@@ -6,7 +6,7 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 30
 
-class Vector():
+class Vector(object):
     # Creates vector based on an input list of coordinates and also sets the dimensions of the space
     # that the vector lives in.
     
@@ -44,7 +44,7 @@ class Vector():
     def normalized(self):
         try:
             magnitude = self.magnitude()
-            return self.times_scalar(1./magnitude)
+            return self.times_scalar(Decimal(1.0)/Decimal(magnitude))
  
         except ZeroDivisionError:
             raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
@@ -82,12 +82,95 @@ class Vector():
 
     def is_zero(self, tolerance=1e-10):
         return self.magnitude() < tolerance
-        
+
+    def orthogonal_to(self, v):
+        try:
+            proj = self.projection_of(v)
+            return self.minus(proj)
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    def projection_of(self, v): 
+        try:
+            u1 = v.normalized()
+            u2 = self.dot_product(u1)
+            return u1.times_scalar(u2)
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    def cross_product(self, v):
+        try: 
+            x1, y1, z1 = self.coordinates
+            x2, y2, z2 = v.coordinates
+            cross = [(y1*z2-y2*z1), -(x1*z2-x2*z1), (x1*y2-x2*y1)]
+            return Vector(cross)
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross_product(v_embedded_in_R3)
+            elif (msg == 'too many values to unpack' or 
+                    msg == 'need more than 1 value to unpack'):
+                raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIM_MSG)
+            else:
+                raise e
+
+    def prlgrm_area(self, v):
+        u1 = self.cross_product(v)
+        p_a = u1.magnitude()
+        return p_a
+
+    def tri_area(self, v):
+        return 0.5*(self.prlgrm_area(v))
+
     def __str__(self):
         return 'Vector: {}' .format(self.coordinates)
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+
+print 'Cross Product'
+v = Vector([8.462, 7.893, -8.187])
+w = Vector([6.984, -5.975, 4.778])
+print v.cross_product(w)
+
+print '\nParallelogram area'
+v = Vector([-8.987, -9.838, 5.031])
+w = Vector([-4.268, -1.861, -8.866])
+print v.prlgrm_area(w)
+
+print '\nTriangle area'
+v = Vector([1.5, 9.547, 3.691])
+w = Vector([-6.007, 0.124, 5.772])
+print v.tri_area(w)
+
+"""
+print '#1'
+v = Vector([3.039, 1.879])
+w = Vector([0.825, 2.036])
+print v.projection_of(w)
+
+print '\n#2'
+v = Vector([-9.88, -3.264, -8.159])
+w = Vector([-2.155, -9.353, -9.473])
+print v.orthogonal_to(w)
+
+print '\n#3'
+v = Vector([3.009, -6.172, 3.692, -2.51])
+w = Vector([6.404, -9.144, 2.759, 8.718])
+vpar = v.projection_of(w)
+vort = v.orthogonal_to(w)
+print 'Parallel: ', vpar
+print 'Orthogonal: ', vort
+
 
 print('1st')
 v = Vector([-7.579, -7.88])
@@ -109,7 +192,6 @@ v = Vector([2.118, 4.827])
 w = Vector([0, 0])
 print('Is orthogonal: ', v.is_orthogonal_to(w), 'Is parallel: ', v.is_parallel_to(w))
 
-"""
 v = Vector([7.887, 4.138])
 w = Vector([-8.802, 6.776])
 print(v.dot_product(w))
