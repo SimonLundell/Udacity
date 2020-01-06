2d0f6da095a2f314fee2a322ad7a3dc93ef23e71
SimonLundell <simonlundell@hotmail.com>
2020-01-06 14:34:47 +0100

    its fucked

--- line.py
+++ line.py
@@ -23,6 +23,46 @@ class Line(object):
 
         self.set_basepoint()
 
+    def intersection_with(self, ell):
+        try:
+            A, B = self.normal_vector.coordinates
+            C, D = ell.normal_vector.coordinates
+
+            k1 = self.constant_term
+            k2 = ell.constant_term
+
+            x_numerator = D*k1-B*k2
+            y_numerator = C*k1+A*k2
+            one_over_denom = Decimal('1')/(A*D-B*C)
+
+            return Vector([x_numerator, y_numerator]).times_scalar(one_over_denom)
+
+        except ZeroDivisionError:
+            if self == ell:
+                return self
+            else:
+                return None
+    
+    def __eq__(self, ell):
+        
+        if self.normal_vector.is_zero():
+            if not ell.normal_vector.is_zero():
+                return False
+            else:
+                diff = self.constant_term - ell.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif ell.normal_vector.is_zero():
+            return False
+        
+        if not self.is_parallel_to(ell):
+            return False
+        
+        x0 = self.basepoint
+        y0 = ell.basepoint
+        basepoint_difference = x0.minus(y0)
+
+        n = self.normal_vector
+        return basepoint_difference.is_orthogonal_to(n)
 
     def set_basepoint(self):
         try:
@@ -30,8 +70,8 @@ class Line(object):
             c = self.constant_term
             basepoint_coords = ['0']*self.dimension
 
-            initial_index = Line.first_nonzero_index(n)
-            initial_coefficient = n[initial_index]
+            initial_index = Line.first_nonzero_index(n.coordinates)
+            initial_coefficient = n.coordinates[initial_index]
 
             basepoint_coords[initial_index] = c/initial_coefficient
             self.basepoint = Vector(basepoint_coords)
@@ -42,6 +82,11 @@ class Line(object):
             else:
                 raise e
 
+    def is_parallel_to(self, v):
+        n1 = self.normal_vector
+        n2 = v.normal_vector
+
+        return n1.is_parallel_to(n2)
 
     def __str__(self):
 
@@ -95,8 +140,12 @@ class Line(object):
             if not MyDecimal(item).is_near_zero():
                 return k
         raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
-
+           
 
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+ell1 = Line(normal_vector=Vector(['4.046', '2.836']), constant_term='1.21')
+ell2 = Line(normal_vector=Vector(['10.115', '7.09']), constant_term='3.025')
+print 'intersection 1: ', ell1.intersection_with(ell2)
