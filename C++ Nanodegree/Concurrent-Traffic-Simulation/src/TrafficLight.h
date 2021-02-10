771b6a1f4963e1273276b00d5319b3d0343dfb91
SimonLundell <simonlundell@hotmail.com>
2021-02-10 15:34:45 +0100

    FP 1

--- C++ Nanodegree/Concurrent-Traffic-Simulation/src/TrafficLight.h
+++ C++ Nanodegree/Concurrent-Traffic-Simulation/src/TrafficLight.h
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <deque>
 #include <condition_variable>
+#include <string>
 #include "TrafficObject.h"
 
 // forward declarations to avoid include cycle
@@ -30,14 +31,19 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum TrafficLightPhase {red, green};
+
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
-
+    TrafficLight();
     // getters / setters
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
 
 private:
     // typical behaviour methods
@@ -48,6 +54,9 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    virtual void cycleThroughPhases();
+    TrafficLightPhase _currentPhase;
+    
 };
 
 #endif
