b0ee66cc4cafac345c54c19d6c74b6fe9607a8f5
SimonLundell <simonlundell@hotmail.com>
2021-02-14 21:09:43 +0100

    Capstone folder

--- C++ Nanodegree/Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ C++ Nanodegree/Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -3,6 +3,7 @@
 #include "TrafficLight.h"
 #include <future>
 #include <queue>
+#include <ctime>
 
 /* Implementation of class "MessageQueue" */
 
@@ -14,9 +15,7 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function.
     std::unique_lock<std::mutex> uLock(_mtx);
-    //std::cout << "timeout3\n";
     _cond.wait(uLock, [this](){ return !_queue.empty(); });
-    //std::cout << "timeout4\n";
     T item = std::move(_queue.back());
     _queue.clear();
 
@@ -29,10 +28,8 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> sLock(_mtx);
-    _queue.push_back(std::move(msg));
-    _cond.notify_one(); // check this in Udacity class.
-    std::cout << "Thread " << msg << " entered the queue\n";
-    
+    _queue.emplace_back(std::move(msg));
+    _cond.notify_one(); // check this in Udacity class. 
 }
 
 
@@ -74,36 +71,20 @@ void TrafficLight::simulate()
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // define a random generator
     std::random_device rd;
-    // define a distribution
+    std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dist(4.0, 6.0);
-    // create a cycle-time with a true random distribution
-    float cycleTime = dist(rd);
-    // get current time
-    auto start = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed;
-
-    // infinite loop
+    float cycleTime = dist(gen);
+    auto timer = time(0);
     while (true) {
-        // Call sleep for better CPU-usage 
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        // Elapsed time from start
-        elapsed = std::chrono::high_resolution_clock::now() - start;
-        if (elapsed.count() > cycleTime) {
-            // New start when condition is met
-            start = std::chrono::high_resolution_clock::now();
-            // New cycle time
-            cycleTime = dist(rd);
-            // Alternate traffic lights
-            if (_currentPhase == red) {
-                _currentPhase = green;
-            } else {
-                _currentPhase = red;
-            }
-            // move trafficlightphase to the queue
-            auto msg = _currentPhase;
+        auto timer2 = time(0);
+        if (timer2-timer > cycleTime) {
+            _currentPhase == red ? _currentPhase = green : _currentPhase = red;
+            TrafficLightPhase msg = _currentPhase;
             _messageQueue.send(std::move(msg));
+            cycleTime = dist(gen);
+            timer = time(0);
         }
     }
 }
